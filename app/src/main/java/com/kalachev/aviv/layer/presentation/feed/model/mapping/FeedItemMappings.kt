25573cc4d9a5ed3ee1c26dfbcc0c45697757013e
Alex Kalachev <xealstorm@gmail.com>
2024-12-15 23:15:41 +0100

    Proper price formatting

--- app/src/main/java/com/kalachev/aviv/layer/presentation/feed/model/mapping/FeedItemMappings.kt
+++ app/src/main/java/com/kalachev/aviv/layer/presentation/feed/model/mapping/FeedItemMappings.kt
@@ -2,6 +2,7 @@ package com.kalachev.aviv.layer.presentation.feed.model.mapping
 
 import com.kalachev.aviv.layer.domain.model.Flat
 import com.kalachev.aviv.layer.presentation.feed.model.FeedItemModel
+import java.math.RoundingMode
 
 class FeedItemMappings {
     fun toPresentation(): (Flat) -> FeedItemModel = {
@@ -9,7 +10,18 @@ class FeedItemMappings {
             id = it.id,
             city = it.city,
             thumbUrl = it.imageUrl,
-            price = it.price.toString(),
+            // The price is formatted as a string with a trailing zero removed if it is a whole number
+            // and as a string with two decimal places otherwise.
+            // In an edge-case scenario, if the price is null, it is represented as "N/A" (hardcoded string is used for simplicity).
+            price = if (it.price != null) {
+                if (it.price.stripTrailingZeros().scale() <= 0) {
+                    it.price.toBigInteger().toString()
+                } else {
+                    it.price.setScale(2, RoundingMode.HALF_UP).toPlainString()
+                }
+            } else {
+                "N/A"
+            },
         )
     }
 }
